0c00d436d6273646e7e24d37f622cfbfe4322b80
mohamed <109195335+m0h4md@users.noreply.github.com>
2022-07-13 05:50:20 +0200

    Update mask.py

--- mask.py
+++ mask.py
@@ -6,7 +6,7 @@ print("I AM NOT RESPONSIBLE FOR ANY ILLEGAL USE!")
 try:
   def Power():
     terminal = str(input("root@m0d10:~#"))
-    if terminal == "boody":
+    if terminal == "phish":
         org_link = str(input("Enter Your Link Without The Protocol:\n  "))
         mask = str(input("Mask This:\n  "))
         mskd_link = mask + "@" + org_link
