34cc2be7f7dc54102a110e0750561b3f3c4f4342
mohamed <109195335+m0h4md@users.noreply.github.com>
2022-07-13 05:50:00 +0200

    Add files via upload

---
+++ mask.py
@@ -0,0 +1,21 @@
+import os
+
+if not 'SUDO_UID' in os.environ.keys():
+  exit("USE ROOT")
+print("I AM NOT RESPONSIBLE FOR ANY ILLEGAL USE!")
+try:
+  def Power():
+    terminal = str(input("root@m0d10:~#"))
+    if terminal == "boody":
+        org_link = str(input("Enter Your Link Without The Protocol:\n  "))
+        mask = str(input("Mask This:\n  "))
+        mskd_link = mask + "@" + org_link
+        print(mskd_link)
+    elif terminal == "exit":
+        exit()
+    elif terminal == "clear":
+        os.system("clear")
+  while True:
+    Power()
+except KeyboardInterrupt:
+  exit()
